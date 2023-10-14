b588bdd75a3836c8dead922f599081199d25047a
pranavgv26 <pranav.velmurugan@unifo.in>
2023-10-14 11:18:01 +0530

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",   // all format mentioned in here of src folder(taken all css classs)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
