15e3991abefccc1440fe5ffa96a83d01623367f0
pranavgv26 <pranav.velmurugan@unifo.in>
2023-11-23 18:50:42 +0530

    mongoDb compas localy connection setup completed

--- api/index.js
+++ api/index.js
@@ -2,22 +2,37 @@ import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from './routes/auth.route.js'
+import User from "./modle/userModule.js";
 dotenv.config()
 
-mongoose
+try {
+    setTimeout(function () {
+        mongoose.connect("mongodb://0.0.0.0:27017");
+    }, 60000);
+} catch (error) {
 
-.connect(process.env.MONGO)
-.then(()=>{
-    console.log("Conected Succesfuly");
-})
-.catch((error)=>{
-    console.log("connection error",error);
-})
+}
 
-const app = express();
+    const app = express();
 
-app.listen(3000,()=>{
-    console.log("server running on the port 3000!!");
-})
+    app.use(express.json())
 
-app.use('/api/user',userRouter)
+    app.listen(3000,()=>{
+        console.log("server running on the port 3000!!");
+    })
+
+    app.use('/api/user',userRouter)
+    app.use('/api/auth',authRouter)
+
+    app.use((error,req,res,next)=>{
+        const statusCode = error.code || 500
+        const message = error.message || "Internal Server Error"
+
+        return res.status(statusCode).json(
+            {
+                Success:false,
+                statusCode,
+                message,
+            })
+    })
